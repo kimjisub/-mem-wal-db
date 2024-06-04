2d90be0bdd515fa73dd2ceaecff85295f8e548f5
kimjisub <0226daniel@gmail.com>
2024-06-04 09:33:02 +0900

    ✨ Change pipe to socket

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
+#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -71,58 +73,83 @@ int main(int argc, char *argv[]) {
     const char *wal_filename = argv[1];
     replay_transactions(wal_filename);
 
-    int fd_front_to_core[2];
-    int fd_core_to_front[2];
-    pipe(fd_front_to_core);
-    pipe(fd_core_to_front);
+    int sockfd[2];
+    socketpair(AF_UNIX, SOCK_STREAM, 0, sockfd);
 
     pid_t pid = fork();
     if (pid == 0) {
         // 자식 프로세스 (프론트엔드)
-        close(fd_front_to_core[0]);
-        close(fd_core_to_front[1]);
+        close(sockfd[0]);
         while (1) {
             char transaction[MAX_LINE];
             printf("Enter command: ");
             fgets(transaction, sizeof(transaction), stdin);
-            write(fd_front_to_core[1], transaction, strlen(transaction) + 1);
+            write(sockfd[1], transaction, strlen(transaction) + 1);
 
             // 코어 프로세스로부터 응답 읽기
             char response[MAX_LINE];
-            read(fd_core_to_front[0], response, sizeof(response));
+            read(sockfd[1], response, sizeof(response));
             printf("%s\n", response);
         }
     } else {
         // 부모 프로세스 (코어)
-        close(fd_front_to_core[1]);
-        close(fd_core_to_front[0]);
+        close(sockfd[1]);
         char buffer[MAX_LINE];
-        while (read(fd_front_to_core[0], buffer, sizeof(buffer)) > 0) {
+        while (read(sockfd[0], buffer, sizeof(buffer)) > 0) {
             char *command, *key, *value;
-            command = strtok(buffer, " ");
-            key = strtok(NULL, " ");
-            value = strtok(NULL, " ");
+            command = strtok(buffer, " \n");
+            for (char *p = command; *p; ++p)
+                *p = toupper(*p);
 
             if (command != NULL) {
-                if (strcmp(command, "SET") == 0 && key != NULL &&
-                    value != NULL) {
-                    set_value(key, value);
-                } else if (strcmp(command, "DEL") == 0 && key != NULL) {
-                    del_value(key);
+                if (strcmp(command, "SET") == 0) {
+                    key = strtok(NULL, " \n");
+                    value = strtok(NULL, " \n");
+                    if (key != NULL && value != NULL) {
+                        set_value(key, value);
+                        write(sockfd[0], "OK", 3);
+                    } else {
+                        write(sockfd[0], "INVALID COMMAND", 15);
+                    }
+                } else if (strcmp(command, "DEL") == 0) {
+                    key = strtok(NULL, " \n");
+                    if (key != NULL) {
+                        del_value(key);
+                        write(sockfd[0], "OK", 3);
+                    } else {
+                        write(sockfd[0], "INVALID COMMAND", 15);
+                    }
                 } else if (strcmp(command, "FLUSHALL") == 0) {
-                    // FLUSHALL 명령어 처리
-                } else if (strcmp(command, "GET") == 0 && key != NULL) {
-                    char result[MAX_LINE];
-                    get_value(key, result, sizeof(result));
-                    write(fd_core_to_front[1], result, strlen(result) + 1);
-                } else if (strcmp(command, "EXIST") == 0 && key != NULL) {
-                    int exists = is_key_exist(key);
-                    char response[2];
-                    snprintf(response, sizeof(response), "%d", exists);
-                    write(fd_core_to_front[1], response, strlen(response) + 1);
+                    write(sockfd[0], "NOT IMPLEMENT", 3);
+                } else if (strcmp(command, "GET") == 0) {
+                    key = strtok(NULL, " \n");
+                    if (key != NULL) {
+                        char result[MAX_LINE];
+                        get_value(key, result, sizeof(result));
+                        write(sockfd[0], result, strlen(result) + 1);
+                    } else {
+                        write(sockfd[0], "INVALID COMMAND", 15);
+                    }
+                } else if (strcmp(command, "EXIST") == 0) {
+                    key = strtok(NULL, " \n");
+                    if (key != NULL) {
+                        int exists = is_key_exist(key);
+                        char response[2];
+                        snprintf(response, sizeof(response), "%d", exists);
+                        write(sockfd[0], response, strlen(response) + 1);
+                    } else {
+                        write(sockfd[0], "INVALID COMMAND", 15);
+                    }
+                } else if (strcmp(command, "HELP") == 0) {
+                    write(sockfd[0], "SET <key> <value> : set value\n", 30);
+                    write(sockfd[0], "GET <key> : get value\n", 22);
+                    write(sockfd[0], "DEL <key> : delete value\n", 25);
+                    write(sockfd[0], "FLUSHALL : delete all values\n", 29);
+                    write(sockfd[0], "EXIST <key> : check if key exists\n", 34);
+                    write(sockfd[0], "HELP : show this message\n", 26);
+                } else {
+                    write(sockfd[0], "INVALID COMMAND", 15);
                 }
-                // 완료되면 OK 응답 보내기
-                write(fd_core_to_front[1], "OK", 3);
             }
         }
         wait(NULL);
